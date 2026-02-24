18475d6456e3b57b6545be93ea2293e510d9f7ec
LilyAna06 <lily@kahuenviro.co.nz>
2026-02-24 19:03:20 +1300

    Update map.js to add a basemap switcher and load geospatial data.

--- LilyTrialMaps/js/map.js
+++ LilyTrialMaps/js/map.js
@@ -1,34 +1,24 @@
-// Include Leaflet CSS
-import 'leaflet/dist/leaflet.css';
-
 // Initialize the map
-const map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([latitude, longitude], zoomLevel);
 
-// Set up the tile layer with attribution
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// Basemap options
+let baseLayers = {
+    "Satellite": L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }),
+    "Street Map": L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', { maxZoom: 17 }),
+    "Terrain": L.tileLayer('https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', { maxZoom: 22 }),
+    "Dark Mode": L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', { maxZoom: 19 })
+};
+
+// Add basemap switcher control
+L.control.layers(baseLayers).addTo(map);
 
 // Load geospatial data
 fetch('path/to/LilysHouse_Cop_FeaturesToJSO.json')
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
         L.geoJSON(data, {
             onEachFeature: function (feature, layer) {
-                layer.bindPopup(feature.properties.name);
+                layer.bindPopup(feature.properties.description);
             }
         }).addTo(map);
     });
-
-// Add zoom controls
-L.control.zoom({ position: 'topright' }).addTo(map);
-
-// Add a title to the map
-map.createPane('title').style.zIndex = 650;
-
-// Ensure the map fits the available space
-document.addEventListener('DOMContentLoaded', function () {
-    map.invalidateSize();
-});
-
-// Code explanation and other functionalities may go here.
